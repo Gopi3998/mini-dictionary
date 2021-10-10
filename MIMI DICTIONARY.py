1df680a54adf643fced2a509ce198d6a305b041b
Gopi3998 <91726200+Gopi3998@users.noreply.github.com>
2021-10-10 11:30:28 -0700

    Add files via upload

---
+++ MIMI DICTIONARY.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+ascii_dict={}
+
+for i in alphabets:
+    ascii_dict[i]=ord(i)
+    
+print(ascii_dict)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
